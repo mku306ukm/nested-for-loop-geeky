bea8a9665fe9cff37836954ec5686471a5218839
samod singh <samod0098@gmail.com>
2021-06-18 13:29:36 +0530

    nested-for-loop-geeky

---
+++ index.js
@@ -0,0 +1,12 @@
+
+for (i = 0; i < 3; i++) {
+    document.write("<strong>outer loop</strong>");
+    document.write(i);
+    document.write("<br>");
+    for (j = 0; j < 5; j++) {
+        document.write("iner loop");
+        document.write(j);
+        document.write("<br>");
+    }
+
+}
